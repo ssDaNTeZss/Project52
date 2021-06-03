16c993465aecde04356fb9099cf5b2c5e7e3aae8
ssDaNTeZss <ss.dantez.ss@gmail.com>
2021-06-04 02:09:11 +0300

    Added topbar

---
+++ src/main/webapp/src/app/topbar/topbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-topbar',
+  templateUrl: './topbar.component.html',
+  styleUrls: ['./topbar.component.less']
+})
+export class TopbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
