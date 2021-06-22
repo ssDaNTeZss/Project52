742ed041603831dbff5d471789b79888ee43d4d6
ssDaNTeZss <ss.dantez.ss@gmail.com>
2021-06-23 00:49:15 +0300

    Services are allocated in a separate folder

--- src/main/webapp/src/app/services/web-request.service.ts
+++ src/main/webapp/src/app/services/web-request.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { environment } from "src/environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,8 @@ export class WebRequestService {
   readonly ROOT_URL;
 
   constructor(private http: HttpClient) {
-    this.ROOT_URL = "http://localhost:3000";
+    // this.ROOT_URL = "http://localhost:3000";
+    this.ROOT_URL = environment.apiUrl;
   }
 
   get(uri: string) {
