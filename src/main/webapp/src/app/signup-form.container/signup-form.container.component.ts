51b1e1e71cae8bad299305abd678049372237025
ssDaNTeZss <ss.dantez.ss@gmail.com>
2021-07-26 02:53:17 +0300

    Updated signup-form

--- src/main/webapp/src/app/signup-form.container/signup-form.container.component.ts
+++ src/main/webapp/src/app/signup-form.container/signup-form.container.component.ts
@@ -1,15 +1,33 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
+import {SignupService} from "../services/signup.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-signup-form',
   templateUrl: './signup-form.container.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SignupFormContainerComponent implements OnInit {
+export class SignupFormContainerComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private subs: Subscription;
+  openSignupForm = false;
 
-  ngOnInit(): void {
+  constructor(
+    private signupService: SignupService,
+    private changeDetection: ChangeDetectorRef,
+  ) {
   }
 
+  ngOnInit(): void {
+    this.signupService.openSignupForm$.subscribe((openSignupForm: boolean) => {
+      this.openSignupForm = openSignupForm;
+      this.changeDetection.markForCheck();
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.subs) {
+      this.subs.unsubscribe();
+    }
+  }
 }
--- src/main/webapp/src/app/signup-form/signup-form.component.ts
+++ src/main/webapp/src/app/signup-form/signup-form.component.ts
@@ -1,15 +1,30 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
 import {AbstractControl, FormControl, FormGroup, Validators} from "@angular/forms";
+import {of, Subscription} from "rxjs";
+import {SignupService} from "../services/signup.service";
+import {confirmPasswordValidator} from "../validators/confirmPassword.validator";
+import {HttpErrorResponse, HttpResponse} from "@angular/common/http";
+import {catchError} from "rxjs/operators";
 
 @Component({
   selector: 'app-signup-form-ui',
   templateUrl: './signup-form.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SignupFormComponent implements OnInit {
+export class SignupFormComponent implements OnInit, OnDestroy {
 
+  private subs: Subscription;
   formModelSignUp: FormGroup;
-  constructor() { }
+  openSignupForm = false;
+  showValidationError = false;
+  validationError: string;
+  showPopup = false;
+
+  constructor(
+    private signupService: SignupService,
+    private changeDetection: ChangeDetectorRef,
+  ) {
+  }
 
   ngOnInit(): void {
     this.formModelSignUp = new FormGroup({
@@ -20,18 +35,20 @@ export class SignupFormComponent implements OnInit {
           Validators.minLength(2)
         ],
       ),
-      password: new FormControl(
-        "",
-        [
-          Validators.required,
-        ],
-      ),
-      reEnterPassword: new FormControl(
-        "",
-        [
-          Validators.required,
-        ],
-      ),
+      passwords: new FormGroup({
+        password: new FormControl(
+          "",
+          [
+            Validators.required,
+          ],
+        ),
+        reEnterPassword: new FormControl(
+          "",
+          [
+            Validators.required,
+          ],
+        )
+      }, [confirmPasswordValidator()]),
       email: new FormControl(
         "",
         [
@@ -40,6 +57,12 @@ export class SignupFormComponent implements OnInit {
         ],
       )
     });
+  }
+
+  ngOnDestroy(): void {
+    if (this.subs) {
+      this.subs.unsubscribe();
+    }
   }
 
   isControlInvalid(controlName: string): boolean {
@@ -54,11 +77,11 @@ export class SignupFormComponent implements OnInit {
   }
 
   get _password(): AbstractControl {
-    return this.formModelSignUp.get("password");
+    return this.formModelSignUp.get("passwords.password");
   }
 
   get _reEnterPassword(): AbstractControl {
-    return this.formModelSignUp.get("reEnterPassword");
+    return this.formModelSignUp.get("passwords.reEnterPassword");
   }
 
   get _email(): AbstractControl {
@@ -66,11 +89,46 @@ export class SignupFormComponent implements OnInit {
   }
 
   onSubmitForm() {
-    console.log(this._username.errors?.minlength)
-    console.log(this._username.errors?.minlength.minlength)
-    console.log(this._username.errors?.minlength.min)
-    console.log(this._username.errors?.minLength)
-    console.log(this._username.errors?.minlength.minLength)
-    console.log(this._username.errors?.minlength.min)
+    const FMS = this.formModelSignUp.value,
+      email = FMS.email,
+      password = FMS.passwords.password,
+      username = FMS.username;
+
+    this.signupService.signup(email, password, username)
+      .pipe(catchError((e: HttpErrorResponse) => {
+        if (e.status === 400) {
+          console.log("400");
+          this.showValidationError = true;
+          this.validationError = e.error;
+          this.changeDetection.markForCheck();
+          return of(null);
+        }
+        if (e.status === 200) {
+          console.log("200");
+          this.showPopup = true;
+          this.changeDetection.markForCheck();
+          return of(null);
+        }
+      }))
+      .subscribe(() => {
+      });
+  }
+
+  closeSignupForm(): void {
+    this.signupService.openSignupForm(false);
+    this.showPopup = false;
+
+    this.changeDetection.markForCheck();
+  }
+
+
+  backHome():void {
+    this.closeSignupForm();
+
+
+  }
+
+  signIn(): void {
+    this.closeSignupForm();
   }
 }
