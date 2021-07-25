44106477168696858316df1d49977cf96d08b7ea
ssDaNTeZss <ss.dantez.ss@gmail.com>
2021-07-26 02:50:17 +0300

    Create confirmPassword.validator.ts

---
+++ src/main/webapp/src/app/validators/confirmPassword.validator.ts
@@ -0,0 +1,19 @@
+import {AbstractControl, ValidationErrors, ValidatorFn} from "@angular/forms";
+
+export function confirmPasswordValidator(): ValidatorFn {
+  return (
+    control: AbstractControl
+  ): { [key: string]: boolean } | null => {
+
+    const password = control.value.password,
+      reEnterPassword = control.value.reEnterPassword;
+
+    if (password.trim() != "" && reEnterPassword.trim() != ""
+      && password !== reEnterPassword) {
+
+      control.get('reEnterPassword').setErrors({ NoPasswordMatch: true });
+      return { NoPasswordMatch: true };
+    }
+    return null;
+  }
+}
