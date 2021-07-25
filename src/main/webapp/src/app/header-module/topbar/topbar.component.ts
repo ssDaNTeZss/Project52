92ca955e238a3f53017e20e00bc49c0e7608ab3f
ssDaNTeZss <ss.dantez.ss@gmail.com>
2021-07-26 02:52:52 +0300

    Updated topbar

--- src/main/webapp/src/app/header-module/topbar/topbar.component.ts
+++ src/main/webapp/src/app/header-module/topbar/topbar.component.ts
@@ -1,5 +1,6 @@
 import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {OpenNavbarService} from "../../services/open-navbar.service";
+import {SignupService} from "../../services/signup.service";
 
 @Component({
   selector: 'app-topbar',
@@ -9,18 +10,29 @@ import {OpenNavbarService} from "../../services/open-navbar.service";
 export class TopbarComponent implements OnInit {
 
   statusNavbar = false;
+  openAccMenu = false;
 
   constructor(
     private changeDetection: ChangeDetectorRef,
     private openNavbarService: OpenNavbarService,
+    private signupService: SignupService,
   ) {  }
 
   ngOnInit(): void {
   }
 
-  openNavbar() {
+  openNavbar(): void {
     this.statusNavbar = !this.statusNavbar;
     this.openNavbarService.openNavbar(this.statusNavbar);
     this.changeDetection.markForCheck();
+  }
+
+  openAccountMenu(): void {
+    this.openAccMenu = !this.openAccMenu;
+  }
+
+  openSignupForm(): void {
+    this.signupService.openSignupForm(true);
+    this.openAccountMenu();
   }
 }
