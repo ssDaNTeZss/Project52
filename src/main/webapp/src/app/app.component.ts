07fb9b57f4acece3dfed4486d6d3982d74d6bdda
ssDaNTeZss <ss.dantez.ss@gmail.com>
2021-07-26 23:25:54 +0300

    Deleted app.component.less

--- src/main/webapp/src/app/app.component.ts
+++ src/main/webapp/src/app/app.component.ts
@@ -5,7 +5,6 @@ import {environment} from "../environments/environment";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements OnInit {
