8fa3087582d0f4dd5a7961e348c981ebe50dafbc
ssDaNTeZss <ss.dantez.ss@gmail.com>
2021-07-29 02:13:34 +0300

    Updated login service

--- src/main/webapp/src/app/services/login.service.ts
+++ src/main/webapp/src/app/services/login.service.ts
@@ -9,9 +9,25 @@ import {tap} from "rxjs/operators";
 })
 export class LoginService {
   public openLoginForm$ = new Subject<boolean>();
+  public openLoginPopup$ = new Subject<boolean>();
+  public validErr$ = new Subject<{showValidationError: boolean, validationError: string}>();
+
+  public setUserState$ = new Subject<any>();
+
+  public openLoginPopup(openLoginPopup: boolean): void {
+    this.openLoginPopup$.next(openLoginPopup);
+  }
 
   public openLoginForm(openLoginForm: boolean): void {
     this.openLoginForm$.next(openLoginForm);
+  }
+
+  public validErr(validErr: {showValidationError: boolean, validationError: string}): void {
+    this.validErr$.next(validErr);
+  }
+
+  public setUserState(setUserState: any): void {
+    this.setUserState$.next(setUserState);
   }
 
   constructor(
